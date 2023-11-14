a2187985bfb790d59046ec2a2c8a7dfde601e02d
evanderzzstudio <evanderstudio@gmail.com>
2023-11-14 18:50:30 +0700

    Update event listeners for left element and add hover-left class to container.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const left = document.querySelector('.left')
+const left = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
